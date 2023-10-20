792348f2181ccfabc0fc0edf436d317f4a73de0a
mishasharmaa <148586686+mishasharmaa@users.noreply.github.com>
2023-10-20 15:56:48 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator { // calculator class
+    
+    public double add(double x, double y) // add function 
+    {
+        return x + y; // return the operation
+    }
+    public double multiply(double x, double y) // multiply function 
+    {
+        return x * y; // return the operation 
+    }
+}
